89c189cb62775f605e6a87eecf240692b83e2863
Jessabel-Art <jsantos4@alumni.fullsail.edu>
2025-12-09 23:08:52 -0500

    Prevent dismiss prop from leaking to toast DOM

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -35,12 +35,12 @@ const toastVariants = cva(
 	},
 );
 
-const Toast = React.forwardRef(({ className, variant, ...props }, ref) => {
-	return (
-		<ToastPrimitives.Root
-			ref={ref}
-			className={cn(toastVariants({ variant }), className)}
-			{...props}
+const Toast = React.forwardRef(({ className, variant, dismiss: _dismiss, ...props }, ref) => {
+        return (
+                <ToastPrimitives.Root
+                        ref={ref}
+                        className={cn(toastVariants({ variant }), className)}
+                        {...props}
 		/>
 	);
 });
